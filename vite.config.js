1a8637c35a9b08886c6fb5f88c7badc5976133e5
lucas mendes <lucasmndes.dev@gmail.com>
2025-04-01 13:57:19 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,19 @@
 
 // })
 
-export default {
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
   base: '/youCourse/',
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        404: resolve(__dirname, '404.html') // Adiciona ao build
+        main: './index.html',
+        404: './404.html'
       }
     }
   }
-}
+});
